654d319b1a3ee0287d2b93663f508112ff3841c9
Matthew Vogee <matthewvogee@Matthews-MacBook-Pro.local>
2022-05-03 18:10:47 -0700

    save button sends PATCH request to server to update the spending item

--- client/src/components/PopEditSpending.jsx
+++ client/src/components/PopEditSpending.jsx
@@ -10,8 +10,27 @@ function PopEditSpending(props) {
         purchaseListCopy[props.idxEdit] = newItem;
         props.setPurchaseList(purchaseListCopy);
     }
-    function updateServer() {
+    async function updateServer() {
+        let data = {itemName: props.nameEdit, amount: props.amountEdit, category: props.categoryEdit, date: props.dateEdit, itmId: props.purchaseList[props.idxEdit].id}
         console.log("Updating the server");
+        let url = "/monthSpending";
+        let opts = {
+            method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow', // manual, *follow, error
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data) // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        };
+
+        const response = await fetch(url, opts);
+        const reData = await response.json();
+        console.log(reData);
     }
     function submitBtn(event) {
         event.preventDefault();
