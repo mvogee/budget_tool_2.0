8807c6eda7e3d75f244d92368091b80ef53a36b3
Matthew Vogee <matthewvogee@Matthews-MacBook-Pro.local>
2022-06-01 19:34:57 -0700

    profile page updates and styling

--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
+import "../styles/profile.css";
 
 /* TODO:
 *   - State handlers for forms
 *   - Handler for submit button to update password.
+*   - Use login route to verify password is correct. should get it back in response.success
 */
 
-
-function getUser() {
-    // make authentication call to server.
-    // if user is retrieved display content.
-    // if user is not retrieved redirect user to login page.
-    return ({id: 1, userName: "Mvogee", email: "mvogee@email.com", password: "xlksdjflksdj"});
-}
-
 function Profile(props) {
-
-    let user = getUser();    
 
     function changeEmailButton(event) {
         event.preventDefault();
@@ -30,21 +22,29 @@ function Profile(props) {
     return (
         <div className="profile">
             <h1>Profile</h1>
-            <p>userName: { user.userName }</p>
-            <p>Email: {user.email}</p>
-            <form className="emailChangeForm">
-                <label htmlFor="emailChange">Change email</label>
-                <input id="emailChange" className="emailChangeInput" placeHolder="new email address."></input>
-                <button type="submit" onClick={changeEmailButton}>Change Email</button>
+            <p className="top-stats">User name: {props.user ? props.user.username : "Loading ..."}</p>
+            <p className="top-stats">Email: {props.user ? props.user.email : "Loading ..."}</p>
+            <form className="emailChangeForm form_div">
+                <div className="input_div">
+                    <label htmlFor="emailChange">Change email</label>
+                    <input id="emailChange" className="emailChangeInput" placeholder="new email address."></input>
+                </div>
+                <button type="button" onClick={changeEmailButton}>Update</button>
             </form>
-            <form className="pwChangeForm">
-                <label htmlFor="oldPW">Old password.</label>
-                <input id="oldPW" type="password" placeholder="Your current password"></input>
-                <label htmlFor="newPW">New password</label>
-                <input id="newPW" type="password" placeholder="New password"></input>
-                <label htmlFor="confirmPW">Confirm new password</label>
-                <input id="confirmPW" type="password" placeholder="Retype new password"></input>
-                <button type="submit" onClick={passwordChangeButton}>Change Password</button>
+            <form className="pwChangeForm form_div">
+                <div className="input_div">
+                    <label htmlFor="oldPW">Old password.</label>
+                    <input id="oldPW" type="password" placeholder="Your current password"></input>
+                </div>
+                <div className="input_div">
+                    <label htmlFor="newPW">New password</label>
+                    <input id="newPW" type="password" placeholder="New password"></input>
+                </div>
+                <div className="input_div">
+                    <label htmlFor="confirmPW">Confirm new password</label>
+                    <input id="confirmPW" type="password" placeholder="Retype new password"></input>
+                </div>
+                <button type="button" onClick={passwordChangeButton}>Update</button>
             </form>
         </div>
     );
