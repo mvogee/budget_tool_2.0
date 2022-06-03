568a25e265e3ec28f6f8447a1c7c9f8d9eb8fa46
Matthew Vogee <matthewvogee@Matthews-MacBook-Pro.local>
2022-06-02 19:45:45 -0700

    updated password and update email functionality and styling

--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
+import { useNavigate } from "react-router-dom";
+import checkAuth from "../checkAuth";
 import "../styles/profile.css";
 
 /* TODO:
@@ -8,41 +10,146 @@ import "../styles/profile.css";
 */
 
 function Profile(props) {
+    const [newPw, setNewPw] = useState("");
+    const [confPw, setConfPw] = useState("");
+    const [newEmail, setNewEmail] = useState("");
+    const [message, setMessage] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const authenticate = async () => {
+            let auth = await checkAuth(props.setUser);
+            if (!auth) {
+                navigate("/login");
+            }
+        }
+        authenticate();
+    }, []);
 
     function changeEmailButton(event) {
         event.preventDefault();
-        alert("email was changed");
+        if (window.confirm("Update email?") === true) {
+            changeEmailRequest();
+        }
+        
+    }
+
+    async function changeEmailRequest() {
+        let auth = await checkAuth(props.setUser);
+        if (auth) {
+            const data = {newEmail: newEmail};
+            let url = "/updateEmail";
+            let opts = {
+                method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(data) // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            };
+            const response = await fetch(url, opts);
+            const reData = await response.json();
+            if (reData.success) {
+                setMessage("Email updated!");
+            }
+            else {
+                setMessage("Oops! Something went wrong. Please try again.")
+            }
+            console.log(reData);
+            clearForm();
+        }
+        else {
+            navigate("/login");
+        }
+    }
+
+    async function changePasswordRequest() {
+        let auth = await checkAuth(props.setUser);
+        if (auth) {
+            const data = {newPw: newPw};
+            let url = "/updatePassword";
+            let opts = {
+                method: 'PATCH', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(data) // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            };
+            const response = await fetch(url, opts);
+            const reData = await response.json();
+            if (reData.success) {
+                setMessage("Password updated!");
+            }
+            else {
+                setMessage("Oops! Something went wrong. Please try again.")
+            }
+            console.log(reData);
+            clearForm();
+        }
+        else {
+            navigate("/login");
+        }
     }
 
     function passwordChangeButton(event) {
         event.preventDefault();
-        alert("password was changed");
+        if ((newPw === confPw) && (window.confirm("Change password?") === true)) {
+            changePasswordRequest();
+        }
+        else {
+            if (newPw !== confPw) {
+                setMessage("The passwords did not match. Please try again.");
+            }
+        }
     }
 
+    function clearForm() {
+        setConfPw("");
+        setNewPw("");
+        setNewEmail("");
+    }
+
+    function emailInput(event) {
+        setNewEmail(event.target.value);
+    }
+    function newPwInput(event) {
+        setNewPw(event.target.value);
+    }
+    function confPwInput(event) {
+        setConfPw(event.target.value);
+    }
     return (
         <div className="profile">
             <h1>Profile</h1>
             <p className="top-stats">User name: {props.user ? props.user.username : "Loading ..."}</p>
             <p className="top-stats">Email: {props.user ? props.user.email : "Loading ..."}</p>
+            <h3 className="">{message}</h3>
             <form className="emailChangeForm form_div">
                 <div className="input_div">
                     <label htmlFor="emailChange">Change email</label>
-                    <input id="emailChange" className="emailChangeInput" placeholder="new email address."></input>
+                    <input id="emailChange" type="email" className="emailChangeInput" placeholder="new email address." value={newEmail} onChange={emailInput}></input>
                 </div>
                 <button type="button" onClick={changeEmailButton}>Update</button>
             </form>
             <form className="pwChangeForm form_div">
                 <div className="input_div">
-                    <label htmlFor="oldPW">Old password.</label>
-                    <input id="oldPW" type="password" placeholder="Your current password"></input>
-                </div>
-                <div className="input_div">
                     <label htmlFor="newPW">New password</label>
-                    <input id="newPW" type="password" placeholder="New password"></input>
+                    <input id="newPW" type="password" placeholder="New password" value={newPw} onChange={newPwInput}></input>
                 </div>
                 <div className="input_div">
                     <label htmlFor="confirmPW">Confirm new password</label>
-                    <input id="confirmPW" type="password" placeholder="Retype new password"></input>
+                    <input id="confirmPW" type="password" placeholder="Retype new password" value={confPw} onChange={confPwInput}></input>
                 </div>
                 <button type="button" onClick={passwordChangeButton}>Update</button>
             </form>
